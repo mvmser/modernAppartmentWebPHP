e0deb2f3971466c4cb3685276bb10cc33038b92b
momoserhir <momoserhir@gmail.com>
2018-11-04 21:35:13 +0000

    include footer

--- index.php
+++ index.php
@@ -155,43 +155,7 @@
 </section>
 <!-- END PRESENTATION 3 -->
     
-<!-- FOOTER -->
-<footer class="container-fluid px-0">
-    <div class="topfooter"> 
-            <div class="contact">
-                    <h3>Contact</h3>
-                    <div class="separator" style="margin-bottom:10px;"></div>
-
-                    <div class="mail">
-                        <img class="mailicon" src="img/icons/mail.png" alt="mail">
-                        <a href="mailto:momoserhir@gmail.com">momoserhir@gmail.com</a>
-                    </div>
-                    <div class="phone">
-                        <img class="phoneicon" src="img/icons/phone.png" alt="phone">
-                        <a href="tel:+33698077266">+33698077286</a>
-                    </div>
-            </div>
-
-            <div class="social">
-                    <h3>Social</h3>
-                    <div class="separator" style="margin-bottom:10px;"></div>
-                    
-                    <a href="https://www.linkedin.com/in/mohamed-serhir/" target="_blank"> 
-                        <img class="social_icon" src="img/icons/sLinkedin.png" alt="Linkedin"> 
-                    </a>
-                    <a href="https://www.facebook.com/momoserhir" target="_blank" > 
-                        <img class="social_icon" src="img/icons/sFacebook.png" alt="Facebook"> 
-                    </a>
-                    <a href="https://plus.google.com/u/0/108237308610571460401" target="_blank" > 
-                        <img class="social_icon" src="img/icons/sGoogleplus.png" alt="Googleplus"> 
-                    </a>
-            </div>
-    </div>
-    <div class="copyright">
-      <p> 2018 © Mohamed Serhir </p>
-    </div>
-</footer>
-<!-- END FOOTER -->
+<?php  include("include/footer.php") ?>
 
 <!-- JS -->
     <script src="js/jquery-3.3.1.js"></script>
